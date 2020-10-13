0da52e62b1cf192b057bf91ab4c9f6b30395ab86
pspeegle <pspeegle@crimson.ua.edu>
2020-10-12 21:33:42 -0500

    completed hash

--- lib/hashing.dart
+++ lib/hashing.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
-
-import 'package:csv/csv_settings_autodetection.dart';
-import 'package:reservrec/test_users.dart';
-import 'package:reservrec/main.dart';
-
-import 'package:csv/csv.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/utils/utils.dart';
 import 'package:crypto/crypto.dart';
+
+String Sha256(String password){
+  var bytes_password = utf8.encode(password);
+  var hexed = sha256.convert(bytes_password);
+  return hexed.toString();
+}
