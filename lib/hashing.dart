5e524a703427f1f41aedfe1b14d757611e43c39a
pspeegle <pspeegle@crimson.ua.edu>
2020-10-12 21:17:59 -0500

    hashing functions for password

---
+++ lib/hashing.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:csv/csv_settings_autodetection.dart';
+import 'package:reservrec/test_users.dart';
+import 'package:reservrec/main.dart';
+
+import 'package:csv/csv.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/utils/utils.dart';
+import 'package:crypto/crypto.dart';
