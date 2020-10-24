51242221c209c6096f6aee19840b549a61d685ac
pspeegle <pspeegle@crimson.ua.edu>
2020-10-24 01:20:50 -0500

    ya im griefing mb

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'file:///C:/Users/pspee/StudioProjects/ReservRec/lib/src/main.dart';
+import 'package:reservrec/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
