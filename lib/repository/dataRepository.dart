627b5f2494fa86d7e084c826bbcc8d43ad58e868
pspeegle <pspeegle@crimson.ua.edu>
2020-10-24 01:29:16 -0500

    insane data repository

---
+++ lib/repository/dataRepository.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:reservrec/models/user.dart';
+
+//Top level collection of users will be called 'user'
+
+class DataRepository {
+
+  final CollectionReference collection = FirebaseFirestore.instance.collection('user');
+
+  //the following "querysnapshot" gets real time updates. found this on a stack overflow webpage
+
+  Stream<QuerySnapshot> getStream() {
+    return collection.snapshots();
+  }
+
+  Future<DocumentReference> addUserC(UserC userc) {
+    return collection.add(userc.toJson());
+  }
+
+  updateUser(UserC userc) async {
+    await collection.doc(userc.reference.id).update(userc.toJson());
+  }
+}
