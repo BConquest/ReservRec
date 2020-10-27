5100ffab530bc5f5eeefdaeaa688dc7dc5559453
pspeegle <pspeegle@crimson.ua.edu>
2020-10-27 16:31:57 -0500

    camelCase isSo cringeAtRec

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,41 +1,38 @@
-import 'dart:async';
-import 'package:reservrec/src/file_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:reservrec/src/main.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class UserC {
   //I think it's a good idea to separate the User class and their functions so I can add all of the json stuff we need
-  int user_id;
-  String user_username;
-  String user_password;
-  String user_email;
+  int userId;
+  String userUsername;
+  String userPassword;
+  String userEmail;
   bool verified;
   String school;
 
   DocumentReference reference;
-  UserC(this.user_id, {this.user_username, this.user_password, this.user_email, this.verified, this.school, this.reference});
+  UserC(this.userId, {this.userUsername, this.userPassword, this.userEmail, this.verified, this.school, this.reference});
 
   factory UserC.fromJson(Map<dynamic, dynamic> json) => _UserCFromJson(json);
 
   Map<String, dynamic> toJson() => _UserCToJson(this);
   @override
-  String toString() => "User<$user_id>";
+  String toString() => "User<$userId>";
 
   //i feel like im in 4th grade making these stupid methods
 
   void setUsername(String u){
-    this.user_username = u;
+    this.userUsername = u;
   }
 
   void setPassword(String u){
-    this.user_password = u;
+    this.userPassword = u;
   }
 
   void setEmail(String u){
-    this.user_email = u;
+    this.userEmail = u;
   }
 
   void setVerified(bool u){
@@ -50,9 +47,9 @@ class UserC {
 UserC _UserCFromJson(Map<dynamic, dynamic> json){
   return UserC(
     json['user_id'] as int,
-    user_username: json['user_username'] as String,
-    user_password: json['user_password'] as String,
-    user_email: json['user_email'] as String,
+    userUsername: json['user_username'] as String,
+    userPassword: json['user_password'] as String,
+    userEmail: json['user_email'] as String,
     verified: json['verified'] as bool,
     school: json['school'] as String,
   );
@@ -60,10 +57,10 @@ UserC _UserCFromJson(Map<dynamic, dynamic> json){
 
 Map<String, dynamic> _UserCToJson(UserC instance) =>
   <String, dynamic> {
-      'user_id': instance.user_id,
-      'user_username': instance.user_username,
-      'user_password': instance.user_password,
-      'user_email': instance.user_email,
+      'user_id': instance.userId,
+      'user_username': instance.userUsername,
+      'user_password': instance.userPassword,
+      'user_email': instance.userEmail,
       'verified': instance.verified,
       'school': instance.school,
   };
