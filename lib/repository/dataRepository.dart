7ed70d1ff46c8c1d71258a3a064ec4624fbb66cf
pspeegle <pspeegle@crimson.ua.edu>
2020-10-27 15:10:01 -0500

    update dataRepository.dart

--- lib/repository/dataRepository.dart
+++ lib/repository/dataRepository.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:reservrec/models/post.dart';
+import 'package:reservrec/models/postStatus.dart';
 import 'package:reservrec/models/user.dart';
 
 //Top level collection of users will be called 'user'
@@ -15,6 +17,14 @@ class DataRepository {
 
   Future<DocumentReference> addUserC(UserC userc) {
     return collection.add(userc.toJson());
+  }
+
+  Future<DocumentReference> addPost(Post post) {
+    return collection.add(post.toJson());
+  }
+
+  Future<DocumentReference> addPostStatus(PostStatus post) {
+    return collection.add(post.toJson());
   }
 
   updateUser(UserC userc) async {
