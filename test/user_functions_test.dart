55277dbcc36ac8f98562056876b9c806ebb63a26
pspeegle <pspeegle@crimson.ua.edu>
2020-11-10 03:37:45 -0600

    hashing tests

--- test/user_functions_test.dart
+++ test/user_functions_test.dart
@@ -3,6 +3,7 @@ import 'package:reservrec/src/new_post.dart';
 import 'package:reservrec/src/user_functions.dart';
 import 'package:reservrec/src/verify_location.dart';
 import 'package:reservrec/models/user.dart';
+import 'package:reservrec/src/hashing.dart';
 
 void main() {
   group('Username', () {
@@ -136,8 +137,18 @@ void main() {
   });
 
   group('Hashing Password', (){
-    test('Null', (){
+    //all values were tested with an independent SHA256 string checker
 
+    test('Null', (){
+      expect(Sha256(""), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"); //this is the value of the null string
+    });
+
+    test('hello', (){
+      expect(Sha256("hello"), "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
+    });
+
+    test('hashing a hash value', (){
+      expect(Sha256(Sha256("hello")), "d7914fe546b684688bb95f4f888a92dfc680603a75f23eb823658031fff766d9");
     });
   });
 }
