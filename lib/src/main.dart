857743d0a748cf956a5cf2d0e87c8df108e44043
pspeegle <pspeegle@crimson.ua.edu>
2020-12-03 04:27:31 -0600

    brother momentum.

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text("ReservRec")),
+        appBar: AppBar(title: Text("ReserveRec")),
         body: MessageHandler(),
       ),
     );
