ce0c922511d51b67d4527d7542f11c3cbbbb1ddc
Ryan Carrigan <rpcarrigan@crimson.ua.edu>
2020-12-02 23:46:53 -0600

    new followerNotification function

--- functions/index.js
+++ functions/index.js
@@ -20,3 +20,27 @@ exports.notifyOnNewPost = functions.firestore.document('/posts/{post_id}')
     console.log('Sent Notification')
     return admin.messaging().sendToTopic('posts', notificationContent)
     });
+
+exports.followerNotification = functions.firestore.document('/posts/{post_id}')
+  .onCreate(async (snapshot, context) => {
+    post = snapshot.data();
+    const db = admin.database().ref('/users/');
+    console.log('user_id: ' + post.user_id);
+    const userSnapshot = await db.where('user_id', '==', post.user_id).get();
+    if (userSnapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }
+    userSnapshot.forEach(doc => {
+      console.log('Doc found: ' + doc.user_id);
+    });
+
+    const notificationContent = {
+      notification: {
+        title: 'New post from ' + post.user_id,
+        body: post.description.toString(),
+      }
+    };
+    console.log('Sent Notification')
+    return admin.messaging().sendToTopic(post.user_id, notificationContent)
+    });
